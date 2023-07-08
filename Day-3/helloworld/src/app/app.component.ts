8f10d64a3645eaf101cbea42e17568c1f3e5a479
sunnylalwani41 <sunnylalwani41@gmail.com>
2023-07-08 16:20:25 +0530

    angular project

---
+++ Day-3/helloworld/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div>
+    <h1>Welcome to your life</h1>
+    <hr>
+    <vijay-comp></vijay-comp>
+    
+  </div>
+  `,
+  
+})
+export class AppComponent {
+  title = 'helloworld';
+  
+}
---
+++ Day-3/helloworld/src/app/second.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "vijay-comp",
+    template: `<h2> Hello from Second Component </h2>`
+})
+export class SecondComponent{}
