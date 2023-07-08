fa3ee3c20b7711576ac98adec03fa532ebc0679c
sunnylalwani41 <sunnylalwani41@gmail.com>
2023-07-08 22:25:31 +0530

    angular project update

--- Day-3/steps/src/app/app.component.ts
+++ Day-3/steps/src/app/app.component.ts
@@ -2,9 +2,52 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <h1> {{title}}</h1>
+  <fieldset>
+    <legend>
+      <h2>Attribute Binding</h2>
+      
+    </legend>
+   
+    <h3 [innerHTML]="title"></h3>
+    <h3 [innerText]="title"></h3>
+    <h3 [textContent]="title"></h3>
+    <h3 innerText="title"></h3>
+  </fieldset>
+  <fieldset>
+    <legend>
+      <h2>Property Binding</h2>
+    </legend>
+    <h3 bind-innerHTML="title"></h3>
+    <h3 bind-innerText="title"></h3>
+    <h3 bind-textContent="title"></h3>
+  </fieldset>
+  <fieldset>
+    <legend>
+      <h2> Value Binding</h2>
+    </legend>
+    <input type='text' [value]="title">
+    <input [type]="type">
+  </fieldset>
+  <fieldset>
+    <legend>
+      <h2>Event Binding</h2>
+    </legend>
+    <input #t1 type="text">
+    <button (click)="clickHandler(t1.value)">Click Here</button>
+    <input #num type="number" [value]="count" (change)="count=num.value">
+    <h1 bind-textContent="count"></h1>
+  </fieldset>
+
+  `
+  
 })
 export class AppComponent {
   title = 'steps';
+  type='color';
+  count:any= 5;
+  clickHandler(message:string){
+    alert(message);
+  }
 }
