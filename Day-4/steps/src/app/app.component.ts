9752d01bcf399ab1400bbf1589ad37713aa28f3d
sunnylalwani41 <sunnylalwani41@gmail.com>
2023-07-25 22:30:07 +0530

    angular

--- Day-4/steps/src/app/app.component.ts
+++ Day-4/steps/src/app/app.component.ts
@@ -2,8 +2,38 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: ``
+  template: `
+    <div>
+      <h1> Main Component</h1>
+      <app-child [quantity]=" count">
+        <h3>Heading H3</h3>
+        <h4> Heading H4</h4>
+        <button>Click</button>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro molestias repellat sunt, incidunt omnis minima maxime delectus et optio dignissimos, nam saepe nisi natus magnam aut ea culpa hic expedita.
+        </p>
+        <p class="box">
+          class box
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro molestias repellat sunt, incidunt omnis minima maxime delectus et optio dignissimos, nam saepe nisi natus magnam aut ea culpa hic expedita.
+        </p>
+        <div>{{title}}</div>
+        <div id="apptitle">{{title}}</div>
+        <div>{{title}}</div>
+
+        <ul>
+          <li class=".x">List Item 1</li>
+          <li>List Item 2</li>
+          <li>List Item 3</li>
+          <li>List Item 4</li>
+        </ul>
+        
+      </app-child>
+      <button (click)="count=count+1"> count Increasing {{count}}</button>
+      
+    </div>
+  `
 })
 export class AppComponent {
   title = 'steps';
+  count:number = 4;
 }
--- Day-4/steps/src/app/child.component.ts
+++ Day-4/steps/src/app/child.component.ts
@@ -1,12 +1,34 @@
-import { Component } from "@angular/core";
+// import { outputAst } from "@angular/compiler";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+// import { NodeStyleEventEmitter } from "rxjs/internal/observable/fromEvent";
 
 @Component({
-    selector: "app-child", 
+    selector: "app-child",
+    
     template: `
+    <div>
+        <h2>Child Component</h2>
     
-    
+        <ng-content></ng-content>
+        <hr>
+        <ng-content select="h3"></ng-content>
+        <hr>
+        <ng-content select=".box"></ng-content>
+        <ng-content select=".box"></ng-content>
+        <hr>
+        <ng-content select=".box"></ng-content>
+        <hr>
+        <ng-content select="#apptitle"></ng-content>
+        <hr>
+        <ng-content select="li.x"></ng-content>
+        <h4> quantity: {{quantity}}</h4>
+    </div>
     `
 })
 export class ChildComponent{
+    @Input() quantity:number=0;
+    @Output() childEvent:EventEmitter<any> = new EventEmitter();
+    
+  
 
 }
